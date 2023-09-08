5ce3c93ec86afddb6908ba8292a1ec6da7f1a8e3
meryemctnky <meryemctnkya@gmail.com>
2023-09-08 19:48:44 +0300

    changed redis

--- src/clients/redis.js
+++ src/clients/redis.js
@@ -1,6 +1,7 @@
 const Redis = require('ioredis');
 const redis = new Redis({
-  url: 'rediss://red-cjtjvjp5mpss73aip3e0:FRIkC9qEx4dvIaWvyKg3ok9yU2PB5wbF@oregon-redis.render.com:6379',
+  host: 'rediss://red-cjtjvjp5mpss73aip3e0:FRIkC9qEx4dvIaWvyKg3ok9yU2PB5wbF@oregon-redis.render.com',
+  port: '6379',
 });
 
 export default redis;
